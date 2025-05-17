b0cc27190df66ff6a30fe1a0f0f0e33fb422f176
囧囧有神 <mynameisshiy@gmail.com>
2025-05-17 17:36:14 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["idc.shenligp.com"];
+pub const RS_PUB_KEY: &str = "Dr+R4xStTgnflqvAz+QPp7LhwiXKhNmaz1olBpQRGps=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
